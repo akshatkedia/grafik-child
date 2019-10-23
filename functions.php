3dee0e69e456cb51ee60b784e0be35d10081d67b
Akshat Kedia <akshat.kedia@outlook.com>
2019-10-23 21:02:12 +0530

    add script to footer

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ if ( ! function_exists( 'grafik_qode_child_theme_enqueue_scripts' ) ) {
 		$parent_style = 'grafik-default-style';
 
 		wp_enqueue_style( 'grafik-child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+		wp_enqueue_script( 'grafik-child-script', get_stylesheet_directory_uri() . '/script.js', array( 'jquery' ), false, true );
 	}
 
 	add_action( 'wp_enqueue_scripts', 'grafik_qode_child_theme_enqueue_scripts' );
